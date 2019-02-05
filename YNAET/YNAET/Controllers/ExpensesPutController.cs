c06f1ef6fb22094a0c8be8b20622b3efa5290d85
Johnathan Beal <johnathanbeal@gmail.com>
2019-02-04 21:30:59 -0500

    deeply flawed & questionable put controller code

--- YNAET/YNAET/Controllers/ExpensesPutController.cs
+++ YNAET/YNAET/Controllers/ExpensesPutController.cs
@@ -20,21 +20,64 @@ namespace YNAET.Controllers
         }
 
         [HttpPut("api/expenses/edit/{id}")]
-        public ActionResult Edit(int id)
+        public ActionResult Edit(int id, [FromBody]ExpenseInputModel expenseInputModel)
         {
-            ExpenseModel expense = new ExpenseModel();
+            ExpenseModel expenseModel = new ExpenseModel();
             using (ISession session = _inHibernateSession.OpenSession())
             {
-                expense = session.Query<ExpenseModel>().Where(b => b.Id == id).FirstOrDefault();
+                expenseModel = session.Query<ExpenseModel>().Where(b => b.Id == id).FirstOrDefault();
+                expenseModel.Account = expenseInputModel.Account ?? expenseModel.Account;
+                expenseModel.Category = expenseInputModel.Category ?? expenseModel.Category;
+                expenseModel.ColorCode = expenseInputModel.ColorCode ?? expenseModel.ColorCode;
+                expenseModel.Memo = expenseInputModel.Memo ?? expenseModel.Memo;
+                expenseModel.Payee = expenseInputModel.Payee ?? expenseModel.Payee;
+
+                if (expenseInputModel.Amount == 0)
+                {
+                    expenseModel.Amount = expenseModel.Amount;
+                }
+                else
+                {
+                    expenseModel.Amount = expenseInputModel.Amount;
+                }
+
+                if (expenseInputModel.Date == DateTime.MinValue)
+                {
+                    expenseModel.Date = expenseModel.Date;
+                }
+                else
+                {
+                    expenseModel.Date = expenseInputModel.Date;
+                }
+
+                try
+                { 
+                    expenseModel.Impulse = expenseInputModel.Impulse;
+                }
+                catch
+                {
+                    expenseModel.Impulse = expenseModel.Impulse;
+                }
+
+                try
+                {
+                    expenseModel.Repeat = expenseInputModel.Repeat;
+                }
+                catch
+                {
+                    expenseModel.Repeat = expenseModel.Repeat;
+                }
+               
+
                 using (ITransaction transaction = session.BeginTransaction())//DO I NEED THIS?
                 {
-                    session.SaveOrUpdate(expense);//DO I NEED THIS?
+                    session.SaveOrUpdate(expenseModel);//DO I NEED THIS?
                     transaction.Commit();//DO I NEED THIS?
                 }
             }
 
             ViewBag.SubmitAction = "Save";// DO I NEED THIS?
-            return new JsonResult(expense);
+            return new JsonResult(expenseModel);
         }
     }
 }
